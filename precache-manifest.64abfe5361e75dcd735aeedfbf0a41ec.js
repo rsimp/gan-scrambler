81651e52168793cce2ed746dd666f7fbd70373e5
Robert Simpson <rsimp@wintermute.lan>
2020-09-17 17:27:54 -0500

    Updates

---
+++ precache-manifest.64abfe5361e75dcd735aeedfbf0a41ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0338ecda755b3f363af49a74ffbfac80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a797186183ddf34ce539",
+    "url": "/static/css/main.b962f37a.chunk.css"
+  },
+  {
+    "revision": "8b00b4c35e010f0a4def",
+    "url": "/static/js/2.bb85d537.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bb85d537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a797186183ddf34ce539",
+    "url": "/static/js/main.b5d623a4.chunk.js"
+  },
+  {
+    "revision": "73b60c437eb5d88af646",
+    "url": "/static/js/runtime-main.78b70a87.js"
+  }
+]);
