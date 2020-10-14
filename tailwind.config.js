2894bd493a55bc811bde2dd3472d4472288657dd
Robert Simpson <rsimp@wintermute.lan>
2020-10-14 17:23:55 -0500

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    colors: {
+      primary: {
+        lt: "hsl(231, 50%, 65%)",
+        default: "hsl(231, 50%, 50%)",
+        dk: "hsl(231, 50%, 35%)",
+        txt: "hsl(0, 0%, 100%)",
+      },
+      secondary: {
+        lt: "hsl(4, 90%, 65%)",
+        default: "hsl(4, 90%, 50%)",
+        dk: "hsl(4, 90%, 35%)",
+        txt: "hsl(0, 0%, 100%)",
+      },
+      background: {
+        default: "hsl(0, 0%, 100%)",
+        txt: "hsl(0, 0%, 0%)",
+      },
+      surface: {
+        default: "hsl(0, 0%, 100%)",
+        txt: "hsl(0, 0%, 0%)",
+      },
+      error: {
+        default: "hsl(349, 85%, 40%)",
+        txt: "hsl(0, 0%, 100%)",
+      },
+    },
+    extend: {},
+  },
+  variants: [],
+  plugins: [],
+};
