3107f1b102a73afc43bcdadf7206d08307482a8a
rsimp <dev@rsimp.io>
2020-10-17 00:19:57 -0500

    Add bottom navigation

--- src/app/main-screen/index.tsx
+++ src/app/main-screen/index.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect } from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  BottomNavigation,
+  BottomNavigationAction,
+} from "@material-ui/core";
+import { Shuffle, Layers, Edit } from "@material-ui/icons";
 import { FormattedMessage } from "react-intl";
 
 import { RobotWidgetContainer } from "app/robot-widget";
@@ -7,13 +14,25 @@ import { ConnectedScrambleGenerator } from "app/scramble-generator";
 import { fiveSideSearch } from "app/common/cube/solvers/five-side-solver";
 import styled from "styled-components/macro";
 
-const Screen = styled.div.attrs({ className: "flex flex-col" })``;
+const Screen = styled.div.attrs({ className: "flex flex-col h-screen" })``;
 
 const IconContainer = styled.div.attrs({
   className: "flex flex-row ml-auto",
 })``;
 
+const TabWrapper = styled.div.attrs({
+  className: "flex flex-column w-full mt-auto",
+})``;
+
 export function MainScreen(): JSX.Element {
+  const [navigationValue, setNavigation] = React.useState(0);
+  const handleChange = (
+    event: React.ChangeEvent<unknown>,
+    newValue: number
+  ) => {
+    setNavigation(newValue);
+  };
+
   useEffect(() => {
     fiveSideSearch.initialize();
   }, []);
@@ -31,7 +50,33 @@ export function MainScreen(): JSX.Element {
         </Toolbar>
       </AppBar>
 
-      <ConnectedScrambleGenerator />
+      <div className="flex flex-col h-full">
+        <ConnectedScrambleGenerator />
+        <TabWrapper>
+          <BottomNavigation
+            showLabels
+            value={navigationValue}
+            onChange={handleChange}
+            className="w-full"
+          >
+            <BottomNavigationAction
+              icon={<Shuffle />}
+              label="RANDOM"
+              aria-label="phone"
+            />
+            <BottomNavigationAction
+              icon={<Layers />}
+              label="CFOP"
+              aria-label="favorite"
+            />
+            <BottomNavigationAction
+              icon={<Edit />}
+              label="MANUAL"
+              aria-label="person"
+            />
+          </BottomNavigation>
+        </TabWrapper>
+      </div>
     </Screen>
   );
 }
