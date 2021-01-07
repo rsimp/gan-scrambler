c61b24d81d4904e2eb73e3bfdd9126f70766dc13
rsimp <dev@rsimp.io>
2021-01-07 10:59:50 -0600

    Fix root Saga logic, add runSaga to store

--- src/app/common/store.ts
+++ src/app/common/store.ts
@@ -1,8 +1,7 @@
 import { Reducer } from "redux";
 import { configureStore, EnhancedStore } from "@reduxjs/toolkit";
-import createSagaMiddleware from "redux-saga";
-import { SagaIterator } from "redux-saga";
-import { call, spawn, delay } from "redux-saga/effects";
+import createSagaMiddleware, { Saga, Task, SagaIterator } from "redux-saga";
+import { call, spawn, delay, all } from "redux-saga/effects";
 
 // ApplicationState will be dynamically populated from on-startup.ts scripts
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -16,35 +15,39 @@ export function registerReducer<TKey extends keyof ApplicationState>(
   reducerRegistry[key] = reducer;
 }
 
-export type Saga = (...args: unknown[]) => SagaIterator;
 const sagaRegistry: Saga[] = [];
 
 export function registerSagas(sagas: Saga[]): void {
   sagaRegistry.push(...sagas);
 }
 
-function* makeRestartable(saga: Saga) {
-  while (true) {
-    try {
-      yield call(saga);
-      console.error("Unexpected root saga termination", saga);
-    } catch (e) {
-      console.error("Saga error, the saga will be restarted", e);
-    }
+function* makeRestartable(saga: Saga): SagaIterator {
+  try {
+    yield call(saga);
+  } catch (e) {
+    console.error("Saga error, the saga will be restarted", e);
     yield delay(500);
+    yield call(makeRestartable, saga);
   }
 }
 
 function* rootSaga() {
-  yield sagaRegistry.map((saga) => spawn(makeRestartable, saga));
+  yield all(sagaRegistry.map((saga) => spawn(makeRestartable, saga)));
 }
 
-export function createStore(): EnhancedStore {
+interface AsyncStore extends EnhancedStore {
+  runSaga: <S extends Saga>(saga: S, ...args: Parameters<S>) => Task;
+}
+
+export function createStore(): AsyncStore {
   const sagaMiddleware = createSagaMiddleware();
   const store = configureStore({
     reducer: reducerRegistry,
     middleware: [sagaMiddleware],
   });
   sagaMiddleware.run(rootSaga);
-  return store;
+  return {
+    ...store,
+    runSaga: sagaMiddleware.run,
+  };
 }
