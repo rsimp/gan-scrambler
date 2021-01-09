86b413e636b09fb458279e79050a3ee50076a1df
rsimp <dev@rsimp.io>
2021-01-09 17:42:15 -0600

    Use builtins for switching on action type in saga

--- src/app/common/snackbar/sagas.ts
+++ src/app/common/snackbar/sagas.ts
@@ -1,36 +1,29 @@
 import { ProviderContext } from "notistack";
 import { takeEvery } from "typed-redux-saga";
 import { SagaIterator } from "redux-saga";
+import { Action } from "@reduxjs/toolkit";
 
 import { enqueueSnackbar, closeSnackbar } from "app/common/snackbar/actions";
 
-type enqueueAction = ReturnType<typeof enqueueSnackbar>;
-type closeAction = ReturnType<typeof closeSnackbar>;
 function* processSnackbarActions(
   snackbar: React.RefObject<ProviderContext>,
-  action: enqueueAction | closeAction
+  action: Action
 ) {
-  if (isEnqueueAction(action)) {
+  if (enqueueSnackbar.match(action)) {
     snackbar.current?.enqueueSnackbar(
       action.payload.message,
       action.payload.options
     );
-  } else {
+  } else if (closeSnackbar.match(action)) {
     snackbar.current?.closeSnackbar(action.payload.key);
   }
-}
-
-function isEnqueueAction(
-  action: enqueueAction | closeAction
-): action is enqueueAction {
-  return action.type === enqueueSnackbar.type;
 }
 
 export function* watchSnackbarActions(
   snackbar: React.RefObject<ProviderContext>
 ): SagaIterator {
   return yield* takeEvery(
-    [enqueueSnackbar, closeSnackbar],
+    [enqueueSnackbar.type, closeSnackbar.type],
     processSnackbarActions,
     snackbar
   );
